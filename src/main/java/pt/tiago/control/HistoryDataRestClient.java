1285e6f0dc0b402629d9b1277b8a0bec0cf31a90
Tiago Martinho <Tiago.Martinho@ctw.bmwgroup.com>
2021-02-06 17:17:28 +0000

    feat(covid-tracker-proxy): change application.properties

--- src/main/java/pt/tiago/control/HistoryDataRestClient.java
+++ src/main/java/pt/tiago/control/HistoryDataRestClient.java
@@ -9,7 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.time.LocalDate;
 
 @RegisterRestClient(configKey="covid.tracker.api")
 @RegisterClientHeaders(HistoryDataRestClientHeadersFactory.class)
